25a725d44afd28fcbc2d041043c18c1cd04f67e6
raj2852 <microsoftrajdip@gmail.com>
2024-12-12 20:07:12 +0530

    kharidlo backend initial setup with spring boot

---
+++ kharidlo/src/main/java/com/kharidlo/kharidlo/KharidloApplication.java
@@ -0,0 +1,13 @@
+package com.kharidlo.kharidlo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KharidloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KharidloApplication.class, args);
+	}
+
+}
---
+++ kharidlo/src/test/java/com/kharidlo/kharidlo/KharidloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kharidlo.kharidlo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KharidloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
